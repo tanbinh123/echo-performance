b6739bc80fff5c13c3ac42ec848d8556c311a06d
tomo0111 <tohito0430@gmail.com>
2018-07-12 22:33:55 +0900

    add entity

--- entity/item.go
+++ entity/item.go
@@ -1,1 +1,10 @@
 package entity
+
+import "time"
+
+type Item struct {
+	ID        int        `gorm:"primary_key" json:"id"`
+	Name      string     `json:"name"`
+	Category  string     `json:"category"`
+	CreatedAt time.Time  `json:"created_at"`
+}
