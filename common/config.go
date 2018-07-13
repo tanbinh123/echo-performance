be8daf398a535df52999c46f290b87de2b943b90
tomo0111 <tohito0430@gmail.com>
2018-07-13 09:47:03 +0900

    add env variable

--- common/config.go
+++ common/config.go
@@ -2,15 +2,20 @@ package common
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"os"
 )
 
-var Db *gorm.DB
+var (
+	Db *gorm.DB
+)
 
 func InitDB() {
-	db, _ := gorm.Open("mysql", os.Getenv("DB_SOURCE"))
-	defer db.Close()
+	db, err := gorm.Open("mysql", os.Getenv("DB_SOURCE"))
+	if err != nil {
+		panic(err)
+	}
 
+	db.DB()
 	Db = db
 }
