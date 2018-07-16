82ad57b62880db3c6d7525e8e33cfea0a1a08325
tomo0111 <tohito0430@gmail.com>
2018-07-16 10:07:58 +0800

    fix query variable

--- controller/item_controller.go
+++ controller/item_controller.go
@@ -3,11 +3,12 @@ package controller
 import (
 	"net/http"
 	"github.com/labstack/echo"
-	"github.com/tomo0111/echo-performance/common"
 	"github.com/tomo0111/echo-performance/entity"
+	"github.com/tomo0111/echo-performance/common"
 )
 
 func ItemController (c echo.Context) error {
 	var items []entity.Item
-	return c.JSON(http.StatusOK, common.Db.Find(&items))
+	common.Db.Find(&items)
+	return c.JSON(http.StatusOK, &items)
 }
